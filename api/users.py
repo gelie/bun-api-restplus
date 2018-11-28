e9099db1af45908991db6f051a3e64753d358d81
gelie <gavin.elie@gmail.com>
2018-11-28 16:48:24 +0200

    Added pagination to user view

--- api/users.py
+++ api/users.py
@@ -1,31 +1,51 @@
-from base64 import b64encode
+from flask import jsonify
+from marshmallow import fields
+from webargs.flaskparser import use_kwargs
 
 from core.models import User
 from . import api
 from .schema import UserSchema
 
-from flask import jsonify
-
 user_schema = UserSchema()
 users_schema = UserSchema(many=True)
 
+pagination = {
+    'page': fields.Int(missing=1, location="query"),
+    'per_page': fields.Int(missing=10, location="query")
+}
+
+filter = {
+    'login': fields.String(missing=None, location="query")
+}
+
 
 @api.route('/users/', methods=['GET'])
-# @api.marshal_list_with(users_schema)
-def get_users():
-    '''List all users'''
-    # return jsonify(dict(name='Gavin', surname='Elie'))
-    users = User.query.paginate(1, 10, False).items
-    for user in users:
-        if user.image is not None:
-            user.image = b64encode(user.image)
-    return jsonify(users_schema.dump(users).data)
+@use_kwargs(filter)
+@use_kwargs(pagination)
+# @use_args(user_schema)
+def get_users(page, per_page, **kwargs):
+    """List all users"""
+    # only = args["fields"]#, None)
+    # print(only)
+    filter = {}
+    for k, v in kwargs.items():
+        filter[k] = v
+    print(filter)
+    if filter.items():
+        users = User.query.filter(filter.items()).paginate(page, per_page, False).items
+    else:
+        users = User.query.paginate(page, per_page, False).items
+    result = users_schema.dump(users)
+    return jsonify(result.data)
+    # for user in users:
+    #     if user.image is not None:
+    #         user.image = b64encode(user.image)
+    # # return jsonify(users_schema.dump(users).data)
+    # return users_schema.dump(users).data
 
 
 @api.route('/users/<user_id>', methods=['GET'])
 def get_user(user_id):
-    # @api.marshal_with(user_schema, skip_none=True)
-
-    '''Fetch a user given its identifier'''
+    """Fetch a user given its identifier"""
     user = User.query.get_or_404(user_id)
     return jsonify(user_schema.dump(user).data)
